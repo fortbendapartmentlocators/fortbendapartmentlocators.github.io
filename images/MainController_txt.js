4e88c3ed0956146052b9d6af06533b5ae37a5a71
FBAl <fortbendapartmentlocators@gmail.com>
2017-05-17 23:02:42 -0500

    Save

--- images/MainController_txt.js
+++ images/MainController_txt.js
@@ -2,27 +2,28 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'Our Community Projects'; 
   $scope.promo = 'Let\'s make meaningful and lasting changes around us';
   $scope.products = [
-  	{ 
-    	name: 'Fort Bend Pets Alive', 
-    	price: 15, 
-    	pubdate: new Date('2017', '05', '01'), 
-    	cover: 'images/FBPA.jpg',
-    	likes: 0,
-  	}, 
-  	{ 
-    	name: 'TextbookConsultants', 
-    	price: 5,
-    	pubdate: new Date('2017', '05', '01'), 
-    	cover: 'images/TB_logo.jpg',
-    	likes: 0,
-  	}, 
-  	{ 
-    	name: 'Fort Bend Apartment Locators', 
-    	price: 10,
-    	pubdate: new Date('2017', '01', '01'), 
-    	cover: 'images/image_1.png',
-    	likes: 0,
-  	}
+
+	{ 
+	    cover: 'images/FBPA.jpg', 
+	    name: 'Fort Bend Pets Alive', 
+	  }, 
+	{ 
+	    cover: 'images/SOCA.png', 
+	    name: 'Saving Our Companion Animals', 
+	  }, 
+	  { 
+	    cover: 'images/peanut.png', 
+	    name: 'A Place for Peanuts', 
+	  },
+	  {
+	    cover: 'images/logo_8.png',
+	    name: 'Fort Bend Apartment Locators',
+	  },
+	  {
+	    cover: 'images/TB_logo.png',
+	    name: 'Textbook Consultants',
+
+	  }
   ];
   $scope.plusOne = function(index) { 
   	$scope.products[index].likes += 1; 
