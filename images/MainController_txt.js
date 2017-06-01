ab3b508267be80a646bf1642cbad0fdbe45990a2
mungs93 <mungs9@gmail.com>
2017-06-01 18:47:58 -0400

    Save

--- images/MainController_txt.js
+++ images/MainController_txt.js
@@ -24,21 +24,6 @@ app.controller('MainController', ['$scope', function($scope) {
 	    url2: "https://www.facebook.com/aplaceforpeanut/",
 	    url3: "https://www.paypal.me/APFP"
 	  },
-	  {
-	    cover: 'images/logo_8.png',
-	    name: 'Fort Bend Apartment Locators',
-	    url: "https://www.fortbendapartmentlocators.com",
-		url2: "https://www.facebook.com/Fort-Bend-Apartment-Locators-170813793441790",
-		url3: "https://www.fortbendapartmentlocators.com"  
-	  },
-	  {
-	    cover: 'images/TB_logo.png',
-	    name: 'Textbook Consultants',
-	    url: "https://textbookconsultants.com",
-	    url2: "https://www.facebook.com/textbookconsultants",
-	    url3: "https://www.paypal.com/donate/?token=GmcFyeJHCDpcdDaSvUdzVVqvmJ4gVoIjvFSDRPaDGL9-zwPjgG8cyIlAwmtBOfzC9QATm0"
-
-	  }
   ];
   $scope.plusOne = function(index) { 
   	$scope.products[index].likes += 1; 
